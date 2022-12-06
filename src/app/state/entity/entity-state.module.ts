5bf7a95fa318078b9d278c5a018fd52f09792506
kevin-tat <31783598+kevin-tat@users.noreply.github.com>
2022-12-06 14:23:31 -0500

    updated module

--- src/app/state/entity/entity-state.module.ts
+++ src/app/state/entity/entity-state.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core'
+import { EffectsModule } from '@ngrx/effects'
+import { StoreModule } from '@ngrx/store'
+import { ProductEntityEffects } from './product-entity/product-entity.effects'
+import { productEntityReducer } from './product-entity/product-entity.reducer'
+import { PRODUCT_ENTITY_STATE_NAME } from './product-entity/product-entity.state'
 
 @NgModule({
   imports: [
     // TODO: Wire up product entity state module
-    StoreModule.forFeature( ... ),
-    // EffectsModule.forFeature( ... ),
+    StoreModule.forFeature(PRODUCT_ENTITY_STATE_NAME, productEntityReducer),
+    EffectsModule.forFeature([ProductEntityEffects]),
   ],
 })
 export class EntityStateModule {}
