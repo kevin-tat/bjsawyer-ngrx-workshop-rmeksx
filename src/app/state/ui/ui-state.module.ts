3befac2e8be81c5cea88a2fefa894cc8ed5ac6bb
Kevin Tat <kevin.tat@zoominfo.com>
2022-12-06 14:35:40 -0500

    changed ui state module

--- src/app/state/ui/ui-state.module.ts
+++ src/app/state/ui/ui-state.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core'
+import { EffectsModule } from '@ngrx/effects'
+import { StoreModule } from '@ngrx/store'
+import { ProductUiEffects } from './product-ui/product-ui.effects'
+import { productUiReducer } from './product-ui/product-ui.reducer'
+import { PRODUCT_UI_STATE_NAME } from './product-ui/product-ui.state'
 
 @NgModule({
   imports: [
     // TODO: Wire up product UI state module
-    // StoreModule.forFeature( ... ),
-    // EffectsModule.forFeature( ... ),
+    StoreModule.forFeature(PRODUCT_UI_STATE_NAME, productUiReducer),
+    EffectsModule.forFeature([ProductUiEffects]),
   ],
 })
 export class UiStateModule {}
